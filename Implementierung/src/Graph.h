3f4b2c13806e23a355984a028c1f4efe4edfb234
Raldir <rami-aly@hotmail.de>
2017-08-15 21:35:17 +0200

    first implementation of Graph

--- Implementierung/src/Graph.h
+++ Implementierung/src/Graph.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "Edge.h";
+#include "Vertex.h";
+
 class Graph
 {
 public:
 	Graph();
 	~Graph();
+
+private:
+	std::vector<Vertex*> filterSpawner();
+
+	std::vector<Vertex*> _spawner;
+
+
+	std::vector<Edge*> _edges;
+	std:: vector<Vertex*> _vertices;
+
 };
 
