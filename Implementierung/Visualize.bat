540c4d70f12d60f9528a9d4bb8d1ebff193a259f
Raldir <rami-aly@hotmail.de>
2017-08-28 23:41:43 +0200

    visualisation and movie polished and high quality

---
+++ Implementierung/Visualize.bat
@@ -0,0 +1,4 @@
+@echo off
+python D:\OneDrive\Module\Semester4\Parallele_Programmierung\PaPo_SmartCars\Implementierung\Visualisation\vis.py %*
+python D:\OneDrive\Module\Semester4\Parallele_Programmierung\PaPo_SmartCars\Implementierung\Visualisation\movie.py %*
+pause
