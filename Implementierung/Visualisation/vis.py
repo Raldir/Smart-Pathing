0982888388e845aff7590ed9eb17369432b7ae88
Raldir <rami-aly@hotmail.de>
2017-08-28 03:29:20 +0200

    whatever

--- Implementierung/Visualisation/vis.py
+++ Implementierung/Visualisation/vis.py
@@ -1,11 +1,16 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import glob
+import cv2
+from PIL import Image
+import numpy
+import os
 
 files = glob.glob('../SmartCarsSequentiell/Output/*.txt')
-totalcount = 0
 for file in files:
     G = nx.DiGraph()
+    path = os.path.basename(open(str(file), 'r').name).split('.',1)[0]
+    #print(os.path.basename(open(str(file), 'r').name).split('.',1)[0])
     pos2 = {}
     labels={}
     f = open('nodes', 'r')
@@ -67,5 +72,25 @@ for file in files:
     nx.draw(G, pos, node_size = 2, edge_size = 2, edge_color = colors)
     #pos2=nx.spring_layout(G)
     nx.draw_networkx_labels(G, pos2, labels, font_size = 9)
-    plt.savefig('visofSteps/' +str(totalcount)+ ' .png')
-    totalcount+=1
+    plt.savefig('visofSteps/' + path  + '.png')
+
+filesImage = glob.glob('visofSteps/*.png')
+Images = {}
+for file in filesImage :
+    #print(str(file))
+    index = os.path.basename(open(str(file), 'r').name).split('.',1)[0]
+    strIndex = str(index)
+    Images[strIndex] = Image.open(str(file))
+
+height, width, layers =  numpy.array(Images['1']).shape
+video = cv2.VideoWriter("demo3_4.avi", # Filename
+                        -1, # Negative 1 denotes manual codec selection. You can make this automatic by defining the "fourcc codec" with "cv2.VideoWriter_fourcc"
+                        10, # 10 frames per second is chosen as a demo, 30FPS and 60FPS is more typical for a YouTube video
+                        (width,height) # The width and height come from the stats of image1
+                        )
+
+for i in range(1,len(filesImage) + 1):
+    for j in range (0, 6) :
+        video.write(cv2.cvtColor(numpy.array(Images[str(i)]), cv2.COLOR_RGB2BGR))
+
+video.release()
