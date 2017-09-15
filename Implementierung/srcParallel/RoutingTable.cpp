7b87c13242860bab45feb5e0cf5cc435078aeebb
Raldir <rami-aly@hotmail.de>
2017-09-15 22:21:35 +0200

    forgot one header

--- Implementierung/srcParallel/RoutingTable.cpp
+++ Implementierung/srcParallel/RoutingTable.cpp
@@ -1,5 +1,14 @@
-#include "RoutingTable.h"
+/*############################################
 
+Author: Rami Aly and Christoph Hueter
+Date : 20.09.17
+Libraries and Licences:
+Boost Library 1.60, MPI Boost Library 1.60, Open Streetmaps and MPI in use.
+All used Maps are licensed under the Open Street Maps License.
+
+#############################################*/
+
+#include "RoutingTable.h"
 #include <boost/graph/use_mpi.hpp>
 #include <boost/config.hpp>
 #include <boost/throw_exception.hpp>
